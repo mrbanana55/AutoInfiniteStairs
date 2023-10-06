807892a093d19da336eca43abe169a2d66d74f43
Andres Delgado <dlegadoandres0502@gmail.com>
2023-10-05 21:46:36 -0600

    changed the whole code

--- main.py
+++ main.py
@@ -2,30 +2,58 @@ import pyautogui
 import time
 import keyboard
 
-#left stair position x: 864 y: 715
-#right stair position x: 1004 y: 715
+#right stair position x: 1004 y: 762
 #color RGB (102, 85, 85)
 
 
-while keyboard.is_pressed('q') == False:
+#checks wether the first step is to the right or to the left
+def checkInitialDirection():
+    if pyautogui.pixel(864, 762)[0] == 102 and pyautogui.pixel(864, 762)[1] == 85 and pyautogui.pixel(864, 762)[2] == 85:
+        direction = "left"
+    elif pyautogui.pixel(1004, 762)[0] == 102 and pyautogui.pixel(1004, 762)[1] == 85 and pyautogui.pixel(1004, 762)[2] == 85:
+        direction = "right"
 
-    time.sleep(10)
-    
-    #press z to make the first step
-    pyautogui.press('z')
+    return direction
 
-    #checks if there is a stair to the left
-    if pyautogui.pixel(864, 715)[0] == 102 and pyautogui.pixel(864, 715)[1] == 85 and pyautogui.pixel(864, 715)[2] == 85:
-        pyautogui.press('z')
-        time.sleep(0.5)
-    else: 
-        pyautogui.press('x')
-        time.sleep(0.5)
-
-    #checks if there is a stair to the right
-    if pyautogui.pixel(1004, 715)[0] == 102 and pyautogui.pixel(864, 715)[1] == 85 and pyautogui.pixel(864, 715)[2] == 85:
-        pyautogui.press('z')
-        time.sleep(0.5)
+def changeDirection(direction):
+    if direction == "left":
+        direction = "right"
     else:
-        pyautogui.press('x')
-        time.sleep(0.5)
+        direction = "left"
+    return direction
+
+def goAhead(direction): #press z to make one step in case it sees one stair ahead. If it doesn't then it changes the direction
+    if direction == "left":
+        if pyautogui.pixel(864, 762)[0] == 102 and pyautogui.pixel(864, 762)[1] == 85 and pyautogui.pixel(864, 762)[2] == 85:
+            pyautogui.press('z')
+            direction = "left"
+
+        else:
+            pyautogui.press('x')
+            changeDirection(direction)
+            direction = "right"
+
+    elif direction == "right":
+        if pyautogui.pixel(1004, 762)[0] == 102 and pyautogui.pixel(1004, 762)[1] == 85 and pyautogui.pixel(1004, 762)[2] == 85:
+            pyautogui.press('z')
+            direction = "right"
+
+        else:
+            pyautogui.press('x')
+            changeDirection(direction)
+            direction = "left"
+    return direction
+#if true, keep the same direction. If false, change direction
+
+#main code
+time.sleep(3) #waits 3 seconds before it starts working
+initial_direction = checkInitialDirection()
+#press z to make the first step
+pyautogui.press('z')
+time.sleep(0.5)
+go_ahead = goAhead(initial_direction)
+time.sleep(0.5)
+while keyboard.is_pressed('q') == False:
+    go_ahead = goAhead(go_ahead)
+    time.sleep(0.5)
+
